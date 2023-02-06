6b9536d451ec145c79feef27cd36ef87115ee8d9
jaemin <datactress@gmail.com>
2023-02-06 22:26:17 +0900

    feat: simple rust blockchain

--- simple-blockchain/src/main.rs
+++ simple-blockchain/src/main.rs
@@ -3,10 +3,10 @@ use models::blockchain::Blockchain;
 use std::{io, error::Error};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Please input a difficulty: ");
+    println!("Please input a difficulty (default: 1)");
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
-    let difficulty = input.trim().parse::<usize>()?;
+    let difficulty = input.trim().parse::<usize>().unwrap_or(1);
 
     let mut blockchain = Blockchain::new(difficulty);
     Blockchain::add_block(&mut blockchain);
