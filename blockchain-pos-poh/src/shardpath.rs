3d522cd6bfec11796ee41bd6866b8ab8b2166083
jaemin <datactress@gmail.com>
2023-03-18 23:49:23 +0900

    feat: update shardpath

--- blockchain-pos-poh/src/shardpath.rs
+++ blockchain-pos-poh/src/shardpath.rs
@@ -2,170 +2,120 @@ use std::{
     collections::HashMap,
     fs,
     time::{Duration, Instant},
+    path::Path,
+    io::BufReader,
 };
+use std::collections::HashSet;
+use std::path::PathBuf;
 
 use rocksdb::{DB, Options, WriteBatch, WriteOptions, IteratorMode};
+use serde::Deserialize;
 
 const MAX_NUM_SHARDS: usize = 128;
 const SHARD_SIZE: usize = 25_000_000;
 const SHARDS_PER_PATH: usize = 32;
 const REINDEX_INTERVAL: u64 = 3600; // Reindex every 1 hour
 
-pub struct ShardPath {
-    pub high_level_shard_index: u16,
-    pub low_level_shard_index: u16,
-    pub path: String,
+const SHARD_INDICES: [(u16, u16); 65536] = {
+    let mut shard_indices = [(0, 0); 65536];
+    for i in 0..256 {
+        for j in 0..256 {
+            let index = i * 256 + j;
+            shard_indices[index] = (i as u16, j as u16);
+        }
+    }
+    shard_indices
+};
+
+#[derive(Deserialize)]
+struct ShardConfig {
+    shard_prefix: String,
+    num_shards_per_path: u16,
+    shard_size: u32,
 }
 
-pub struct ShardIndex {
+pub struct ShardPath {
     inner: HashMap<(u16, u16), String>,
     last_reindexed: Instant,
 }
 
-impl ShardIndex {
+impl ShardPath {
     pub fn new(paths: &[&str]) -> Self {
-        let mut index = ShardIndex {
+        let mut index = ShardPath {
             inner: HashMap::new(),
             last_reindexed: Instant::now(),
         };
 
-        index.add_shard_path(Some(paths));
+        index.index_shards(paths);
         index
     }
 
-    fn index_shard_paths(&mut self) {
-        // Sort shard paths alphabetically
-        let mut sorted_paths = self.inner.values().collect::<Vec<&String>>();
-        sorted_paths.sort();
+    pub fn get_shard(&mut self, account_id: &[u8]) -> Option<&String> {
+        let (high_level_shard_index, low_level_shard_index) = (account_id[0] as u16, account_id[1] as u16);
 
-        // Compute the number of shards and the size of each shard range
-        let num_shards = sorted_paths.len() as u16;
-        let range_size = 256 / num_shards;
-
-        // Update the index for each shard path
-        for (i, path) in sorted_paths.iter().enumerate() {
-            // Compute the shard index based on the number of shards and the position of the path in the sorted list
-            let high_level_shard_index = (i as u16 * range_size) / 256;
-            let low_level_shard_index = (i as u16 * range_size) % 256;
-
-            // Update the shard index of the inner HashMap
-            self.inner.insert((high_level_shard_index, low_level_shard_index), path.to_string());
+        // 샤드를 이미 할당하여 인덱싱 해뒀으면 shard 리턴
+        if let Some(shard) = self.inner.get(&(high_level_shard_index, low_level_shard_index)) {
+            // Reindex periodically if necessary
+            if self.last_reindexed.elapsed() > Duration::from_secs(REINDEX_INTERVAL) {
+                let val = self.inner.values().to_owned();
+                let values = val.into_iter().map(|(k, v)| v.split("/shards/").next().unwrap()).collect::<HashSet<&str>>();
+                self.index_shards(&Vec::from(values));
+                self.last_reindexed = Instant::now();
+            };
+            return Some(shard)
         }
 
+        None
+    }
+
+    pub fn index_shards(&mut self, paths: &[&str]) {
+        // Clear the current index
+        self.inner.clear();
+
+        // Load the shard configuration from file
+        let config_file = fs::File::open(Path::new("src/configmap/shard_config.json")).unwrap();
+        let config_reader = BufReader::new(config_file);
+        let config: ShardConfig = serde_json::from_reader(config_reader).unwrap();
+
+        // Compute the size of each shard range
+        let range_size = 65536 / config.num_shards_per_path as u16;
+
+        // Compute the shard ID for each shard range
+        let mut range_to_shard = vec![0; 65536];
+        for i in 0..65536 {
+            range_to_shard[i] = (i / range_size) as u16;
+        } // [0; 2048, 1; 2048,..., 30; 2048, 31; 2048]
+
+        // Assign each range to a shard
+        let mut shard_ranges = vec![vec![]; config.num_shards_per_path as usize];
+        for i in 0..65536 {
+            shard_ranges[range_to_shard[i] as usize].push(i as u16);
+        } // shard_ranges = [[0..2048], [2048..4096],..., [61440..63488], [63488..65536]]
+
+        // Assign a path to each shard
+        let mut shard_paths = vec![];
+        for root_path in paths {
+            for i in 0..config.num_shards_per_path {
+                shard_paths.push(format!("{}/shards/{}{:03}", root_path, config.shard_prefix, i));
+            }
+        } // shard_paths = [somepath[0..32],..., otherpath[0..32]]
+
+        // Distribute the ranges to the shards, ensuring roughly equal number of ranges per shard
+        let mut shard_index = 0;
+        let mut shard_start_range = 0;
+        let mut shard_end_range = 0;
+        for i in 0..65536 {
+            let shard_id = range_to_shard[i];
+            if shard_id != shard_index {
+                self.inner.insert((shard_start_range / 256, shard_start_range % 256), shard_paths[shard_index as usize].clone());
+                shard_index += 1;
+                shard_start_range = i as u16;
+            }
+            shard_end_range = i as u16;
+        }
+        self.inner.insert((shard_start_range / 256, shard_start_range % 256), shard_paths[shard_index as usize].clone());
         self.last_reindexed = Instant::now();
     }
-
-    pub fn add_shard_path(&mut self, option_paths: Option<&[&str]>) {
-        // Compute the shard index based on the number of shards and the position of the path in the sorted list
-        // let num_shards = self.inner.len() as u16;
-        // let high_level_shard_index = (num_shards * u8::MAX+1 as u16) / MAX_NUM_SHARDS as u16;
-        // let low_level_shard_index = (num_shards * u8::MAX+1 as u16) % MAX_NUM_SHARDS as u16;
-
-        // Calculate the shard index range for the new paths
-        let range_size = if paths.len() > 0 {
-            MAX_NUM_SHARDS as u16 / paths.len() as u16
-        } else {
-            0
-        };
-
-        let mut all_paths: Vec<String> = self.inner.values().map(|&s| s.clone()).collect();
-        // Create a vector of all the paths, both old and new
-        if let Some(paths) = option_paths {
-            all_paths.extend(paths.iter().map(|&s| s.to_string()));
-        }
-
-        // Sort the paths in lexicographic order
-        all_paths.sort();
-
-        // Iterate through the sorted all paths and insert them into the index HashMap
-        for (i, path) in all_paths.iter().enumerate() {
-            // Calculate the high and low level shard indices for the new path
-            let high_level_shard_index = (i as u16) / SHARDS_PER_PATH as u16;
-            let low_level_shard_index = if range_size > 0 {
-                ((i as u16) % SHARDS_PER_PATH as u16) * range_size
-            } else {
-                0
-            };
-
-            // Find the correct index to insert the new shard path
-            let insert_index = match self.inner.get(&(high_level_shard_index, low_level_shard_index)) {
-                Some(_) => {
-                    println!("Duplicate shard path indices detected");
-                    return
-                },
-                None => {
-                    let mut index = 0;
-                    for (i, path) in self.inner.values().enumerate() {
-                        let (_, current_low_shard_index) = *self.inner.keys().nth(i).unwrap();
-                        if current_low_shard_index > low_level_shard_index {
-                            break;
-                        }
-                        index = i + 1;
-                    }
-                    index
-                }
-            };
-
-            // Insert the shard path at the correct index
-            let index_key = (high_level_shard_index, low_level_shard_index);
-            self.inner.insert(index_key, path.to_string());
-            self.inner.remove(&self.inner.keys().nth(insert_index).unwrap());
-
-            // Reindex to ensure deterministic shard indexing
-            self.index_shard_paths();
-        }
-    }
-
-    pub fn get_shard_path(&mut self, account_id: &[u8]) -> Option<&String> {
-        // Compute the shard index based on the account ID
-        let num_shards = self.inner.len() as u16;
-        let high_level_shard_index = (u16::from(account_id[0]) * num_shards as u16) / u8::MAX+1;
-        let low_level_shard_index = (u16::from(account_id[1]) * num_shards as u16) / u8::MAX+1;
-
-        // Check if the shard index is greater than or equal to the maximum number of shards
-        if high_level_shard_index >= MAX_NUM_SHARDS as u16 || low_level_shard_index >= MAX_NUM_SHARDS as u16 {
-            return None; // Return None if the shard index exceeds the maximum number of shards
-        }
-
-        // If the shard index is greater than or equal to the current number of shards, add a new shard to the index
-        let shard_index = (high_level_shard_index, low_level_shard_index);
-        if !self.inner.contains_key(&shard_index) {
-            self.add_shard_path(None);
-        }
-
-        // Reindex periodically if necessary
-        if self.last_reindexed.elapsed() > Duration::from_secs(REINDEX_INTERVAL) {
-            self.index_shard_paths();
-            self.last_reindexed = Instant::now();
-        }
-
-        // Get the shard path for the corresponding shard index
-        self.inner.get(&(high_level_shard_index, low_level_shard_index)).map(|s| s.as_str())
-    }
-}
-
-pub fn create_shard_paths(root_path: &str) -> Vec<ShardPath> {
-    let mut shard_paths = Vec::new();
-    for i in 0..SHARDS_PER_PATH {
-        let path = format!("{}/shards/shard_{:03}", root_path, i);
-        fs::create_dir_all(&path).expect("Failed to create shard directory");
-        shard_paths.push(ShardPath {
-            high_level_shard_index: i as u16 / 256,
-            low_level_shard_index: i as u16 % 256,
-            path: path,
-        });
-    }
-    // Calculate the total size of each shard path
-    let mut shard_sizes: Vec<(usize, &ShardPath)> = Vec::with_capacity(shard_paths.len());
-    for shard_path in &shard_paths {
-        let shard_size = get_shard_size(&shard_path.path);
-        shard_sizes.push((shard_size, shard_path));
-    }
-
-    // Sort shard paths by size
-    shard_sizes.sort_by_key(|&(size, _)| size);
-
-    shard_paths
 }
 
 fn get_shard_size(path: &str) -> usize {
